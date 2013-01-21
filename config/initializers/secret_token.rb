6bc29c30207bac6577ea71a7a9c6fcf6f51a06db
Ken Seal <kas@ncgr.org>
2013-01-21 13:01:44 -0700

    Secret token is set to env var or generated randomly.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-LisSequenceSearch::Application.config.secret_token = '7fdb4821678b5e8e6e6643c357a21832fa36475b1c52e574de8ab85967abcc14ab63821259372a8397a1e1e67d009c7c240f2c19fe92543b5244a3d9b01e18c5'
+LisSequenceSearch::Application.config.secret_token = ENV['LIS_SECRET_TOKEN'] || %x{ openssl rand -hex 64 }
