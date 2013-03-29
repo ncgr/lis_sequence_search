edd05e6ad1f9c524eb7829333626f925b5af2d48
Alex Rice <agr@ncgr.org>
2013-03-29 14:12:23 -0600

    Found uncommitted changes to this script. Also added some comments.

--- start_redis_server.bash
+++ start_redis_server.bash
@@ -1,10 +1,15 @@
-#!/bin/bash
+#!/bin/bash -e
+
+# note: the medplants site is not part of LIS, but it shares a redis
+# server with search.comparative-legumes.org (because of the Quorum
+# gem). only one of either medplants or LIS search should start the
+# redis server from init scripts. --agr 2013-03
 
 WORKERS=$1
 
 if [[ ! $WORKERS =~ [[:digit:]] ]]; then
-    echo "Error: please enter the number of resque workers."
-    exit 1
+    echo "Number of resque workers not provided. Starting 4..."
+    WORKERS=4
 fi
 
 # Start redis server
